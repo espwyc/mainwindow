7d502a71f3f158088bce94f3b4b894c9bb514881
richard <314941534@qq.com>
2018-05-19 15:27:02 +0800

    180519
    Signed-off-by: richard <314941534@qq.com>

--- GeneratedFiles/Release/moc_mainwindow.cpp
+++ GeneratedFiles/Release/moc_mainwindow.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[43];
-    char stringdata0[548];
+    QByteArrayData data[47];
+    char stringdata0[585];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -40,48 +40,52 @@ QT_MOC_LITERAL(5, 45, 8), // "Web_Jump"
 QT_MOC_LITERAL(6, 54, 3), // "url"
 QT_MOC_LITERAL(7, 58, 11), // "bbt_clicked"
 QT_MOC_LITERAL(8, 70, 11), // "bte_clicked"
-QT_MOC_LITERAL(9, 82, 19), // "correct_btn_clicked"
-QT_MOC_LITERAL(10, 102, 17), // "wrong_btn_clicked"
-QT_MOC_LITERAL(11, 120, 7), // "test_wd"
-QT_MOC_LITERAL(12, 128, 4), // "text"
-QT_MOC_LITERAL(13, 133, 8), // "pic_show"
-QT_MOC_LITERAL(14, 142, 22), // "test_mysql_btn_clicked"
-QT_MOC_LITERAL(15, 165, 25), // "test_showpath_btn_clicked"
-QT_MOC_LITERAL(16, 191, 16), // "hide_btn_clicked"
-QT_MOC_LITERAL(17, 208, 3), // "sig"
-QT_MOC_LITERAL(18, 212, 13), // "sta_tl_update"
-QT_MOC_LITERAL(19, 226, 14), // "m2_1_triggered"
-QT_MOC_LITERAL(20, 241, 14), // "m2_2_triggered"
-QT_MOC_LITERAL(21, 256, 14), // "m2_3_triggered"
-QT_MOC_LITERAL(22, 271, 14), // "m3_1_triggered"
-QT_MOC_LITERAL(23, 286, 14), // "m3_2_triggered"
-QT_MOC_LITERAL(24, 301, 14), // "m4_1_triggered"
-QT_MOC_LITERAL(25, 316, 14), // "m4_2_triggered"
-QT_MOC_LITERAL(26, 331, 14), // "m5_1_triggered"
-QT_MOC_LITERAL(27, 346, 14), // "m5_2_triggered"
-QT_MOC_LITERAL(28, 361, 14), // "m6_1_triggered"
-QT_MOC_LITERAL(29, 376, 16), // "ShowSearchResult"
-QT_MOC_LITERAL(30, 393, 5), // "picid"
-QT_MOC_LITERAL(31, 399, 17), // "ShowResultByPicId"
-QT_MOC_LITERAL(32, 417, 11), // "ul1_clicked"
-QT_MOC_LITERAL(33, 429, 11), // "ul2_clicked"
-QT_MOC_LITERAL(34, 441, 11), // "ul3_clicked"
-QT_MOC_LITERAL(35, 453, 11), // "ul4_clicked"
-QT_MOC_LITERAL(36, 465, 11), // "ul5_clicked"
-QT_MOC_LITERAL(37, 477, 11), // "ul6_clicked"
-QT_MOC_LITERAL(38, 489, 11), // "ul7_clicked"
-QT_MOC_LITERAL(39, 501, 15), // "result_withspan"
-QT_MOC_LITERAL(40, 517, 2), // "ts"
-QT_MOC_LITERAL(41, 520, 15), // "addvideocenterw"
-QT_MOC_LITERAL(42, 536, 11) // "AboutDialog"
+QT_MOC_LITERAL(9, 82, 17), // "wrong_btn_clicked"
+QT_MOC_LITERAL(10, 100, 7), // "test_wd"
+QT_MOC_LITERAL(11, 108, 4), // "text"
+QT_MOC_LITERAL(12, 113, 8), // "pic_show"
+QT_MOC_LITERAL(13, 122, 22), // "test_mysql_btn_clicked"
+QT_MOC_LITERAL(14, 145, 25), // "test_showpath_btn_clicked"
+QT_MOC_LITERAL(15, 171, 16), // "hide_btn_clicked"
+QT_MOC_LITERAL(16, 188, 3), // "sig"
+QT_MOC_LITERAL(17, 192, 13), // "sta_tl_update"
+QT_MOC_LITERAL(18, 206, 14), // "m1_1_triggered"
+QT_MOC_LITERAL(19, 221, 14), // "m2_1_triggered"
+QT_MOC_LITERAL(20, 236, 14), // "m2_2_triggered"
+QT_MOC_LITERAL(21, 251, 14), // "m2_3_triggered"
+QT_MOC_LITERAL(22, 266, 14), // "m3_1_triggered"
+QT_MOC_LITERAL(23, 281, 14), // "m3_2_triggered"
+QT_MOC_LITERAL(24, 296, 14), // "m4_1_triggered"
+QT_MOC_LITERAL(25, 311, 14), // "m4_2_triggered"
+QT_MOC_LITERAL(26, 326, 14), // "m5_1_triggered"
+QT_MOC_LITERAL(27, 341, 14), // "m5_2_triggered"
+QT_MOC_LITERAL(28, 356, 14), // "m6_1_triggered"
+QT_MOC_LITERAL(29, 371, 16), // "ShowSearchResult"
+QT_MOC_LITERAL(30, 388, 5), // "picid"
+QT_MOC_LITERAL(31, 394, 17), // "ShowResultByPicId"
+QT_MOC_LITERAL(32, 412, 11), // "ul1_clicked"
+QT_MOC_LITERAL(33, 424, 11), // "ul2_clicked"
+QT_MOC_LITERAL(34, 436, 11), // "ul3_clicked"
+QT_MOC_LITERAL(35, 448, 11), // "ul4_clicked"
+QT_MOC_LITERAL(36, 460, 11), // "ul5_clicked"
+QT_MOC_LITERAL(37, 472, 11), // "ul6_clicked"
+QT_MOC_LITERAL(38, 484, 11), // "ul7_clicked"
+QT_MOC_LITERAL(39, 496, 15), // "result_withspan"
+QT_MOC_LITERAL(40, 512, 2), // "ts"
+QT_MOC_LITERAL(41, 515, 15), // "addvideocenterw"
+QT_MOC_LITERAL(42, 531, 11), // "AboutDialog"
+QT_MOC_LITERAL(43, 543, 12), // "SubmitQuerry"
+QT_MOC_LITERAL(44, 556, 13), // "QuerryStruct&"
+QT_MOC_LITERAL(45, 570, 2), // "QS"
+QT_MOC_LITERAL(46, 573, 11) // "Show_Status"
 
     },
     "MainWindow\0testemit\0\0bb1_clicked\0"
     "lb1_clicked\0Web_Jump\0url\0bbt_clicked\0"
-    "bte_clicked\0correct_btn_clicked\0"
-    "wrong_btn_clicked\0test_wd\0text\0pic_show\0"
-    "test_mysql_btn_clicked\0test_showpath_btn_clicked\0"
-    "hide_btn_clicked\0sig\0sta_tl_update\0"
+    "bte_clicked\0wrong_btn_clicked\0test_wd\0"
+    "text\0pic_show\0test_mysql_btn_clicked\0"
+    "test_showpath_btn_clicked\0hide_btn_clicked\0"
+    "sig\0sta_tl_update\0m1_1_triggered\0"
     "m2_1_triggered\0m2_2_triggered\0"
     "m2_3_triggered\0m3_1_triggered\0"
     "m3_2_triggered\0m4_1_triggered\0"
@@ -91,7 +95,8 @@ QT_MOC_LITERAL(42, 536, 11) // "AboutDialog"
     "ul1_clicked\0ul2_clicked\0ul3_clicked\0"
     "ul4_clicked\0ul5_clicked\0ul6_clicked\0"
     "ul7_clicked\0result_withspan\0ts\0"
-    "addvideocenterw\0AboutDialog"
+    "addvideocenterw\0AboutDialog\0SubmitQuerry\0"
+    "QuerryStruct&\0QS\0Show_Status"
 };
 #undef QT_MOC_LITERAL
 
@@ -101,7 +106,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      37,   14, // methods
+      39,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -109,45 +114,47 @@ static const uint qt_meta_data_MainWindow[] = {
        1,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    0,  199,    2, 0x06 /* Public */,
+       1,    0,  209,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       3,    0,  200,    2, 0x08 /* Private */,
-       4,    0,  201,    2, 0x08 /* Private */,
-       5,    1,  202,    2, 0x08 /* Private */,
-       7,    0,  205,    2, 0x08 /* Private */,
-       8,    0,  206,    2, 0x08 /* Private */,
-       9,    0,  207,    2, 0x08 /* Private */,
-      10,    0,  208,    2, 0x08 /* Private */,
-      11,    1,  209,    2, 0x08 /* Private */,
-      13,    0,  212,    2, 0x08 /* Private */,
-      14,    0,  213,    2, 0x08 /* Private */,
-      15,    0,  214,    2, 0x08 /* Private */,
-      16,    1,  215,    2, 0x08 /* Private */,
-      16,    0,  218,    2, 0x28 /* Private | MethodCloned */,
-      18,    0,  219,    2, 0x08 /* Private */,
-      19,    0,  220,    2, 0x08 /* Private */,
-      20,    0,  221,    2, 0x08 /* Private */,
-      21,    0,  222,    2, 0x08 /* Private */,
-      22,    0,  223,    2, 0x08 /* Private */,
-      23,    0,  224,    2, 0x08 /* Private */,
-      24,    0,  225,    2, 0x08 /* Private */,
-      25,    0,  226,    2, 0x08 /* Private */,
-      26,    0,  227,    2, 0x08 /* Private */,
-      27,    0,  228,    2, 0x08 /* Private */,
-      28,    0,  229,    2, 0x08 /* Private */,
-      29,    1,  230,    2, 0x08 /* Private */,
-      31,    1,  233,    2, 0x08 /* Private */,
-      32,    0,  236,    2, 0x08 /* Private */,
-      33,    0,  237,    2, 0x08 /* Private */,
-      34,    0,  238,    2, 0x08 /* Private */,
-      35,    0,  239,    2, 0x08 /* Private */,
-      36,    0,  240,    2, 0x08 /* Private */,
-      37,    0,  241,    2, 0x08 /* Private */,
-      38,    0,  242,    2, 0x08 /* Private */,
-      39,    1,  243,    2, 0x08 /* Private */,
-      41,    0,  246,    2, 0x08 /* Private */,
-      42,    0,  247,    2, 0x08 /* Private */,
+       3,    0,  210,    2, 0x08 /* Private */,
+       4,    0,  211,    2, 0x08 /* Private */,
+       5,    1,  212,    2, 0x08 /* Private */,
+       7,    0,  215,    2, 0x08 /* Private */,
+       8,    0,  216,    2, 0x08 /* Private */,
+       9,    0,  217,    2, 0x08 /* Private */,
+      10,    1,  218,    2, 0x08 /* Private */,
+      12,    0,  221,    2, 0x08 /* Private */,
+      13,    0,  222,    2, 0x08 /* Private */,
+      14,    0,  223,    2, 0x08 /* Private */,
+      15,    1,  224,    2, 0x08 /* Private */,
+      15,    0,  227,    2, 0x28 /* Private | MethodCloned */,
+      17,    0,  228,    2, 0x08 /* Private */,
+      18,    0,  229,    2, 0x08 /* Private */,
+      19,    0,  230,    2, 0x08 /* Private */,
+      20,    0,  231,    2, 0x08 /* Private */,
+      21,    0,  232,    2, 0x08 /* Private */,
+      22,    0,  233,    2, 0x08 /* Private */,
+      23,    0,  234,    2, 0x08 /* Private */,
+      24,    0,  235,    2, 0x08 /* Private */,
+      25,    0,  236,    2, 0x08 /* Private */,
+      26,    0,  237,    2, 0x08 /* Private */,
+      27,    0,  238,    2, 0x08 /* Private */,
+      28,    0,  239,    2, 0x08 /* Private */,
+      29,    1,  240,    2, 0x08 /* Private */,
+      31,    1,  243,    2, 0x08 /* Private */,
+      32,    0,  246,    2, 0x08 /* Private */,
+      33,    0,  247,    2, 0x08 /* Private */,
+      34,    0,  248,    2, 0x08 /* Private */,
+      35,    0,  249,    2, 0x08 /* Private */,
+      36,    0,  250,    2, 0x08 /* Private */,
+      37,    0,  251,    2, 0x08 /* Private */,
+      38,    0,  252,    2, 0x08 /* Private */,
+      39,    1,  253,    2, 0x08 /* Private */,
+      41,    0,  256,    2, 0x08 /* Private */,
+      42,    0,  257,    2, 0x08 /* Private */,
+      43,    1,  258,    2, 0x08 /* Private */,
+      46,    0,  261,    2, 0x08 /* Private */,
 
  // signals: parameters
     QMetaType::Void,
@@ -159,12 +166,12 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,   11,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::Int,   17,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   16,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -189,6 +196,8 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void, QMetaType::QString,   40,
     QMetaType::Void,
     QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 44,   45,
+    QMetaType::Void,
 
        0        // eod
 };
@@ -205,15 +214,15 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 3: _t->Web_Jump((*reinterpret_cast< const QUrl(*)>(_a[1]))); break;
         case 4: _t->bbt_clicked(); break;
         case 5: _t->bte_clicked(); break;
-        case 6: _t->correct_btn_clicked(); break;
-        case 7: _t->wrong_btn_clicked(); break;
-        case 8: _t->test_wd((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 9: _t->pic_show(); break;
-        case 10: _t->test_mysql_btn_clicked(); break;
-        case 11: _t->test_showpath_btn_clicked(); break;
-        case 12: _t->hide_btn_clicked((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 13: _t->hide_btn_clicked(); break;
-        case 14: _t->sta_tl_update(); break;
+        case 6: _t->wrong_btn_clicked(); break;
+        case 7: _t->test_wd((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 8: _t->pic_show(); break;
+        case 9: _t->test_mysql_btn_clicked(); break;
+        case 10: _t->test_showpath_btn_clicked(); break;
+        case 11: _t->hide_btn_clicked((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 12: _t->hide_btn_clicked(); break;
+        case 13: _t->sta_tl_update(); break;
+        case 14: _t->m1_1_triggered(); break;
         case 15: _t->m2_1_triggered(); break;
         case 16: _t->m2_2_triggered(); break;
         case 17: _t->m2_3_triggered(); break;
@@ -236,6 +245,8 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 34: _t->result_withspan((*reinterpret_cast< const QString(*)>(_a[1]))); break;
         case 35: _t->addvideocenterw(); break;
         case 36: _t->AboutDialog(); break;
+        case 37: _t->SubmitQuerry((*reinterpret_cast< QuerryStruct(*)>(_a[1]))); break;
+        case 38: _t->Show_Status(); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
@@ -276,13 +287,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 37)
+        if (_id < 39)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 37;
+        _id -= 39;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 37)
+        if (_id < 39)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 37;
+        _id -= 39;
     }
     return _id;
 }
--- GeneratedFiles/Release/moc_status_w.cpp
+++ GeneratedFiles/Release/moc_status_w.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_status_w_t {
-    QByteArrayData data[8];
-    char stringdata0[87];
+    QByteArrayData data[1];
+    char stringdata0[9];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -31,19 +31,10 @@ struct qt_meta_stringdata_status_w_t {
     )
 static const qt_meta_stringdata_status_w_t qt_meta_stringdata_status_w = {
     {
-QT_MOC_LITERAL(0, 0, 8), // "status_w"
-QT_MOC_LITERAL(1, 9, 17), // "tree_item_changed"
-QT_MOC_LITERAL(2, 27, 0), // ""
-QT_MOC_LITERAL(3, 28, 16), // "QTreeWidgetItem*"
-QT_MOC_LITERAL(4, 45, 4), // "item"
-QT_MOC_LITERAL(5, 50, 6), // "column"
-QT_MOC_LITERAL(6, 57, 13), // "update_status"
-QT_MOC_LITERAL(7, 71, 15) // "treeItemChanged"
+QT_MOC_LITERAL(0, 0, 8) // "status_w"
 
     },
-    "status_w\0tree_item_changed\0\0"
-    "QTreeWidgetItem*\0item\0column\0update_status\0"
-    "treeItemChanged"
+    "status_w"
 };
 #undef QT_MOC_LITERAL
 
@@ -53,52 +44,22 @@ static const uint qt_meta_data_status_w[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       3,   14, // methods
+       0,    0, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       1,       // signalCount
-
- // signals: name, argc, parameters, tag, flags
-       1,    2,   29,    2, 0x06 /* Public */,
-
- // slots: name, argc, parameters, tag, flags
-       6,    0,   34,    2, 0x08 /* Private */,
-       7,    2,   35,    2, 0x08 /* Private */,
-
- // signals: parameters
-    QMetaType::Void, 0x80000000 | 3, QMetaType::Int,    4,    5,
-
- // slots: parameters
-    QMetaType::Void,
-    QMetaType::Void, 0x80000000 | 3, QMetaType::Int,    4,    5,
+       0,       // signalCount
 
        0        // eod
 };
 
 void status_w::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    if (_c == QMetaObject::InvokeMetaMethod) {
-        status_w *_t = static_cast<status_w *>(_o);
-        Q_UNUSED(_t)
-        switch (_id) {
-        case 0: _t->tree_item_changed((*reinterpret_cast< QTreeWidgetItem*(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
-        case 1: _t->update_status(); break;
-        case 2: _t->treeItemChanged((*reinterpret_cast< QTreeWidgetItem*(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
-        default: ;
-        }
-    } else if (_c == QMetaObject::IndexOfMethod) {
-        int *result = reinterpret_cast<int *>(_a[0]);
-        void **func = reinterpret_cast<void **>(_a[1]);
-        {
-            typedef void (status_w::*_t)(QTreeWidgetItem * , int );
-            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&status_w::tree_item_changed)) {
-                *result = 0;
-                return;
-            }
-        }
-    }
+    Q_UNUSED(_o);
+    Q_UNUSED(_id);
+    Q_UNUSED(_c);
+    Q_UNUSED(_a);
 }
 
 const QMetaObject status_w::staticMetaObject = {
@@ -123,25 +84,7 @@ void *status_w::qt_metacast(const char *_clname)
 int status_w::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QWidget::qt_metacall(_c, _id, _a);
-    if (_id < 0)
-        return _id;
-    if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 3)
-            qt_static_metacall(this, _c, _id, _a);
-        _id -= 3;
-    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 3)
-            *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 3;
-    }
     return _id;
-}
-
-// SIGNAL 0
-void status_w::tree_item_changed(QTreeWidgetItem * _t1, int _t2)
-{
-    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)), const_cast<void*>(reinterpret_cast<const void*>(&_t2)) };
-    QMetaObject::activate(this, &staticMetaObject, 0, _a);
 }
 QT_WARNING_POP
 QT_END_MOC_NAMESPACE
